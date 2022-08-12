c9096fc2adbca9a40afb8d9ab6f21c1507dbf1a3
darzwaitz <darzwaitz@yahoo.com>
2022-08-12 21:56:37 +0100

    added tailwindCss to project through npm

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from "vue";
+
 // @ is alias for 'src' folder - set in jsconfig.json
 import App from "@/App.vue";
+
+import "@/assets/tailwind.css";
 
 createApp(App).mount("#app");
