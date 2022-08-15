d7b80508eb0d939fd55aefdf2bf40dee6af4999b
darzwaitz <darzwaitz@yahoo.com>
2022-08-15 23:38:39 +0100

    added google font & configured in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
