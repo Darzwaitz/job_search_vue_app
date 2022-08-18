130001690698e63c740bd00d83cfdfdacbafef11
darzwaitz <darzwaitz@yahoo.com>
2022-08-18 18:21:48 +0100

    using setData function - unit test in MainNav component

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -3,9 +3,13 @@ import { mount } from "@vue/test-utils";
 import MainNav from "@/components/MainNav.vue";
 
 describe("MainNav", () => {
-  it("displays company name", () => {
+  it("displays company name", async () => {
     // wrapper as title is used as convention in Vue because it is the wrapper or container
     const wrapper = mount(MainNav);
-    expect(wrapper.text()).toMatch("Careers");
+    // NB setData is async
+    await wrapper.setData({
+      company: "Super Corp",
+    });
+    expect(wrapper.text()).toMatch("Super Corp");
   });
 });
