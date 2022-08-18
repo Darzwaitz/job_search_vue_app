d1e36cc4e4ee376df1e6a5803d3ca45f97dc3ed1
darzwaitz <darzwaitz@yahoo.com>
2022-08-18 18:23:08 +0100

    deleted setData function - unit test in MainNav component

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -3,13 +3,9 @@ import { mount } from "@vue/test-utils";
 import MainNav from "@/components/MainNav.vue";
 
 describe("MainNav", () => {
-  it("displays company name", async () => {
+  it("displays company name", () => {
     // wrapper as title is used as convention in Vue because it is the wrapper or container
     const wrapper = mount(MainNav);
-    // NB setData is async
-    await wrapper.setData({
-      company: "Super Corp",
-    });
-    expect(wrapper.text()).toMatch("Super Corp");
+    expect(wrapper.text()).toMatch("Careers");
   });
 });
