251a7a5e23d42c05a6c9c8b8757a7b4afd96b2d4
darzwaitz <darzwaitz@yahoo.com>
2022-08-23 19:19:52 +0100

    added new component ProfileImage

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,18 @@
+<template>
+  <img
+    :src="imageLink"
+    alt="Profile picture"
+    class="w-8 h-8 object-contain rounded-3xl"
+  />
+</template>
+
+<script>
+export default {
+  name: "ProfileImage",
+  data() {
+    return {
+      imageLink: "../assets/imgs/profile-pic-pexels.jpg",
+    };
+  },
+};
+</script>
