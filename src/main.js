245bec7d1111a438b0f39b44022de2b8d5bcb464
darzwaitz <darzwaitz@yahoo.com>
2022-08-31 12:16:30 +0100

    installed FontAwesome libaries and imported to main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import { createApp } from "vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 // @ is alias for 'src' folder - set in jsconfig.json
 import App from "@/App.vue";
 
 import "@/assets/tailwind.css";
 
-createApp(App).mount("#app");
+library.add(faSearch);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
