3867e1782559e67377dd24f250c94a768691dd77
darzwaitz <darzwaitz@yahoo.com>
2022-08-31 12:27:10 +0100

    added first fontawesome icon to Subnav component

--- src/components/Subnav.vue
+++ src/components/Subnav.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="w-full h-16 bg-white border-b border-solid border-brand-gray-1">
-    <div class="flex items-center h-full px-8">subnav</div>
+    <div class="flex items-center h-full px-8">
+      <div class="">
+        <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
+        <span><span class="text-brand-green-1">1234</span> jobz matched</span>
+      </div>
+    </div>
   </div>
 </template>
 
