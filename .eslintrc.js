10eaab7022abfa67d445325e43fd81ee708ec80d
darzwaitz <darzwaitz@yahoo.com>
2022-08-31 11:56:51 +0100

    added Subnav component and styling

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "vue/singleline-html-element-content-newline": "off",
     "vue/multiline-html-element-content-newline": "off",
+    "vue/multi-word-component-names": "off",
   },
   overrides: [
     {
