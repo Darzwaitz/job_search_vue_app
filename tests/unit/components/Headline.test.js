c11d564443708f927185dd0fcb1d510d6a7d13f4
darzwaitz <darzwaitz@yahoo.com>
2022-09-12 17:38:18 +0100

    ..added another test for Headline component

--- tests/unit/components/Headline.test.js
+++ tests/unit/components/Headline.test.js
@@ -1,6 +1,7 @@
 import { mount } from "@vue/test-utils";
 
 import Headline from "@/components/Headline";
+import { nextTick } from "vue";
 
 describe("Headline", () => {
   it("displays introductory action verb", () => {
@@ -20,13 +21,21 @@ describe("Headline", () => {
     jest.useRealTimers();
   });
 
-  it("swapz action verb after fitrst interval", () => {
+  it("swapz action verb after fitrst interval", async () => {
     jest.useFakeTimers("legacy");
     const wrapper = mount(Headline);
     jest.runOnlyPendingTimers();
-
+    await nextTick();
     const actionPhrase = wrapper.find("[data-test='action-phrase']");
     expect(actionPhrase.text()).toBe("Create for everyone");
+    jest.useRealTimers();
+  });
+
+  it("removez interval when component disapears", () => {
+    jest.useFakeTimers("legacy");
+    const wrapper = mount(Headline);
+    wrapper.unmount();
+    expect(clearInterval).toHaveBeenCalled();
     jest.useRealTimers();
   });
 });
