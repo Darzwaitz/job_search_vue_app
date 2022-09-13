ea43bebe081a752116790bf3d4b33aaf898a62c8
darzwaitz <darzwaitz@yahoo.com>
2022-09-13 22:27:01 +0100

    adding Job Search Form styling - Part 1 - & font awesome icon

--- src/components/JobSearchForm.vue
+++ src/components/JobSearchForm.vue
@@ -1,5 +1,9 @@
 <template>
-  <form>Test form here..</form>
+  <form
+    class="flex items-center w-full h-12 mt-14 border border-solid border-brand-gray-3 rounded-3xl"
+  >
+    <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3" />
+  </form>
 </template>
 
 <script>
