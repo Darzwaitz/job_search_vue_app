4de1ab5ec2d38fef9ffc21eb8853bdcab9e60383
darzwaitz <darzwaitz@yahoo.com>
2022-09-21 23:02:37 +0100

    writing unit test for TextInput - part 2

--- tests/unit/components/shared/TextInput.test.js
+++ tests/unit/components/shared/TextInput.test.js
@@ -9,5 +9,10 @@ describe("TextInput", () => {
       },
     });
     const input = wrapper.find("input");
+    input.setValue("N");
+    input.setValue("NY");
+    input.setValue("NYC");
+    const messages = wrapper.emitted()["update:modelValue"];
+    expect(messages).toEqual([["N"], ["NY"], ["NYC"]]);
   });
 });
