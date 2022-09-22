1c82136b71f08a30f06d96fd559a00872f3fce96
darzwaitz <darzwaitz@yahoo.com>
2022-09-22 18:27:23 +0100

    creating vue router config file (index.js in router folder)

---
+++ src/views/JobResultsView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1 class="text-6xl font-bold">Job resultz view</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "JobResultzView",
+};
+</script>
