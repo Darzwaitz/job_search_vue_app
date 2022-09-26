5c7926a86fb89b6de8da0b259221052e8a3963db
darzwaitz <darzwaitz@yahoo.com>
2022-09-26 22:33:11 +0100

    routing with $router.push

--- src/views/JobResultsView.vue
+++ src/views/JobResultsView.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="about">
     <h1 class="text-6xl font-bold">Job resultz view</h1>
+    <button @click="goToHome">Go to home page</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "JobResultzView",
+  methods: {
+    goToHome() {
+      this.$router.push({ name: "Home" }); // path: "/"
+    },
+  },
 };
 </script>
