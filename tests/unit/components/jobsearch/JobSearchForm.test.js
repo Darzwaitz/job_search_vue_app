7809c5171563f8800834f5ff68c92c107daf4872
darzwaitz <darzwaitz@yahoo.com>
2022-10-03 23:49:08 +0100

    adding unit test for JobSearchForm - TBU

--- tests/unit/components/jobsearch/JobSearchForm.test.js
+++ tests/unit/components/jobsearch/JobSearchForm.test.js
@@ -4,6 +4,17 @@ import JobSearchForm from "@/components/jobsearch/JobSearchForm";
 
 describe("JobSearchForm", () => {
   describe("when user submitz form", () => {
-    it("");
+    it("directz user to job resultz page with userz search parameterz", () => {
+      const wrapper = mount(JobSearchForm, {
+        global: {
+          stubs: {
+            FontAwesomeIcon: true,
+          },
+        },
+      });
+
+      const roleInput = wrapper.find("[data-test='role-input']");
+      roleInput.setValue();
+    });
   });
 });
