f41b7b79832bd3bf4ca2afa63c75e51e9b9943ca
darzwaitz <darzwaitz@yahoo.com>
2022-10-03 23:43:29 +0100

    adding unit test for JobSearchForm route

---
+++ tests/unit/components/jobsearch/JobSearchForm.test.js
@@ -0,0 +1,9 @@
+import { mount } from "@vue/test-utils";
+
+import JobSearchForm from "@/components/jobsearch/JobSearchForm";
+
+describe("JobSearchForm", () => {
+  describe("when user submitz form", () => {
+    it("");
+  });
+});
