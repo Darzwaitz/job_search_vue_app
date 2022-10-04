099af298654d981bc271a468e8ef90b1e5c4aa3e
darzwaitz <darzwaitz@yahoo.com>
2022-10-04 22:01:07 +0100

    created JobsListings component

---
+++ src/components/jobresults/JobListings.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>Job listings comp.</main>
+</template>
+
+<script>
+export default {
+  name: "JobListings",
+};
+</script>
--- src/views/JobResultsView.vue
+++ src/views/JobResultsView.vue
@@ -1,13 +1,17 @@
 <template>
   <job-filters-sidebar></job-filters-sidebar>
+  <job-listings></job-listings>
 </template>
 
 <script>
 import JobFiltersSidebar from "@/components/jobresults/jobfilterssidebar/JobFiltersSidebar.vue";
+import JobListings from "@/components/jobresults/JobListings.vue";
+
 export default {
   name: "JobResultzView",
   components: {
     JobFiltersSidebar,
+    JobListings,
   },
 };
 </script>
