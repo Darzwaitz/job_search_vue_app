67699f806ab66b5b2bfa46b8bacc52ecf1f18967
darzwaitz <darzwaitz@yahoo.com>
2022-10-04 15:22:02 +0100

    using attachTo in jest utils to fix false error in test

--- tests/unit/components/jobsearch/JobSearchForm.test.js
+++ tests/unit/components/jobsearch/JobSearchForm.test.js
@@ -4,9 +4,16 @@ import JobSearchForm from "@/components/jobsearch/JobSearchForm";
 
 describe("JobSearchForm", () => {
   describe("when user submitz form", () => {
-    it("directz user to job resultz page with userz search parameterz", () => {
+    it("directz user to job resultz page with userz search parameterz", async () => {
+      const push = jest.fn();
+      const $router = { push };
+
       const wrapper = mount(JobSearchForm, {
+        attachTo: document.body,
         global: {
+          mocks: {
+            $router,
+          },
           stubs: {
             FontAwesomeIcon: true,
           },
@@ -14,7 +21,21 @@ describe("JobSearchForm", () => {
       });
 
       const roleInput = wrapper.find("[data-test='role-input']");
-      roleInput.setValue();
+      await roleInput.setValue("Vue developer");
+
+      const locationInput = wrapper.find("[data-test='location-input']");
+      await locationInput.setValue("Berlin");
+
+      const submitButton = wrapper.find("[data-test='form-submit-button']");
+      await submitButton.trigger("click");
+
+      expect(push).toHaveBeenCalledWith({
+        name: "JobResults",
+        query: {
+          role: "Vue developer",
+          location: "Berlin",
+        },
+      });
     });
   });
 });
