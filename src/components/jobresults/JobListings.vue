466100e81bcb6da09235ac8ae32fec9808a95cbe
darzwaitz <darzwaitz@yahoo.com>
2022-10-05 17:02:54 +0100

    css tailwind stylez on JobListings component

--- src/components/jobresults/JobListings.vue
+++ src/components/jobresults/JobListings.vue
@@ -1,5 +1,27 @@
 <template>
-  <main class="flex-auto p-8 bg-brand-gray-2">Job listings comp.</main>
+  <main class="flex-auto p-8 bg-brand-gray-2">
+    <ol>
+      <li class="mb-7">
+        <router-link
+          to="/jobs/results/1"
+          class="block mx-auto bg-white border border-solid border-brand-gray-2 rounded hover:shadow-gray"
+        >
+          <div class="pt-5 pb-2 mx-8 border-b border-solid border-brand-gray-2">
+            <h2 class="mb-2 text-2xl">Technical program manager</h2>
+
+            <div class="flex flex-row align-middle">
+              <div class="mr-5">
+                <span>Company name</span>
+              </div>
+              <div class="">
+                <span>Berlin, Germany</span>
+              </div>
+            </div>
+          </div>
+        </router-link>
+      </li>
+    </ol>
+  </main>
 </template>
 
 <script>
