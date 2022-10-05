bae86968e00abd65b155438e205f36a0a38bb6c0
darzwaitz <darzwaitz@yahoo.com>
2022-10-05 17:10:39 +0100

    creating structure within JobListings

--- src/components/jobresults/JobListings.vue
+++ src/components/jobresults/JobListings.vue
@@ -18,6 +18,30 @@
               </div>
             </div>
           </div>
+
+          <div class="px-8 py-4">
+            <div>
+              <h3 class="mt-1 mb-2">Qualificationz</h3>
+              <div class="">
+                <ul class="pl-8 list-disc">
+                  <li>Bachelorz degree or equivalent pract experience</li>
+                  <li>5 yearz experience in program management</li>
+                  <li>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Reiciendis consectetur recusandae tempora ab maiores aperiam
+                    nemo labore aliquid. Repudiandae officia culpa minus
+                    voluptates eos?
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div class="mt-2 text-center">
+              <router-link to="/jobs/results/1" class="text-brand-blue-1">
+                Expand
+              </router-link>
+            </div>
+          </div>
         </router-link>
       </li>
     </ol>
