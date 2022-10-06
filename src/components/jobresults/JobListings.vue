ac0d09af839919c1f9e6d6f66174ae24dbb063e6
darzwaitz <darzwaitz@yahoo.com>
2022-10-06 21:39:28 +0100

    using async await with axios

--- src/components/jobresults/JobListings.vue
+++ src/components/jobresults/JobListings.vue
@@ -1,9 +1,7 @@
 <template>
   <main class="flex-auto p-8 bg-brand-gray-2">
     <ol>
-      <job-listing />
-      <job-listing />
-      <job-listing />
+      <job-listing v-for="job in jobs" :key="job.id" :job="job" />
     </ol>
   </main>
 </template>
@@ -22,10 +20,9 @@ export default {
       jobs: [],
     };
   },
-  mounted() {
-    axios.get("http://localhost:3000/jobs").then((response) => {
-      this.jobs = response.data;
-    });
+  async mounted() {
+    const response = await axios.get("http://localhost:3000/jobs");
+    this.jobs = response.data;
   },
 };
 </script>
