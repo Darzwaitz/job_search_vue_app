ab9581adc271907fad6000b701c9af4fe963f467
darzwaitz <darzwaitz@yahoo.com>
2022-10-07 22:35:21 +0100

    creating unit test for JobListing component - TBU

--- tests/unit/components/jobresults/JobListing.test.js
+++ tests/unit/components/jobresults/JobListing.test.js
@@ -1,3 +1,37 @@
-import { mount } from "@vue/test-utils";
+import { mount, RouterLinkStub } from "@vue/test-utils";
 
 import JobListing from "@/components/jobresults/JobListing.vue";
+
+describe("JobListing", () => {
+  it("renders job title", () => {
+    const wrapper = mount(JobListing, {
+      props: {
+        job: {
+          title: "Vue developer",
+        },
+      },
+      global: {
+        stubs: {
+          "router-link": RouterLinkStub,
+        },
+      },
+    });
+    expect(wrapper.text()).toMatch("Vue developer");
+  });
+
+  it("renders job organisation", () => {
+    const wrapper = mount(JobListing, {
+      props: {
+        job: {
+          organization: "AirBnB",
+        },
+      },
+      global: {
+        stubs: {
+          "router-link": RouterLinkStub,
+        },
+      },
+    });
+    expect(wrapper.text()).toMatch("AirBnB");
+  });
+});
