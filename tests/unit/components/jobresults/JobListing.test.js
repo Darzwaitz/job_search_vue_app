066cd4566c1a33d45db151bf0b1b4956739f24c3
darzwaitz <darzwaitz@yahoo.com>
2022-10-10 23:35:27 +0100

    creating unit test for JobListing component - refactored, factory function etc.

--- tests/unit/components/jobresults/JobListing.test.js
+++ tests/unit/components/jobresults/JobListing.test.js
@@ -3,35 +3,35 @@ import { mount, RouterLinkStub } from "@vue/test-utils";
 import JobListing from "@/components/jobresults/JobListing.vue";
 
 describe("JobListing", () => {
+  const createJobProps = (jobProps = {}) => ({
+    title: "Vue developer",
+    organization: "AirBnB",
+    ...jobProps,
+  });
+
+  const createConfig = (jobProps) => ({
+    props: {
+      job: {
+        ...jobProps,
+      },
+    },
+    global: {
+      stubs: {
+        "router-link": RouterLinkStub,
+      },
+    },
+  });
+
   it("renders job title", () => {
-    const wrapper = mount(JobListing, {
-      props: {
-        job: {
-          title: "Vue developer",
-        },
-      },
-      global: {
-        stubs: {
-          "router-link": RouterLinkStub,
-        },
-      },
-    });
+    const jobProps = createJobProps({ title: "Vue developer" });
+
+    const wrapper = mount(JobListing, createConfig(jobProps));
     expect(wrapper.text()).toMatch("Vue developer");
   });
 
   it("renders job organisation", () => {
-    const wrapper = mount(JobListing, {
-      props: {
-        job: {
-          organization: "AirBnB",
-        },
-      },
-      global: {
-        stubs: {
-          "router-link": RouterLinkStub,
-        },
-      },
-    });
+    const jobProps = createJobProps({ organization: "AirBnB" });
+    const wrapper = mount(JobListing, createConfig(jobProps));
     expect(wrapper.text()).toMatch("AirBnB");
   });
 });
