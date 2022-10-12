1afbe845eff53f3241c7641f3c1f2a82fd15c217
darzwaitz <darzwaitz@yahoo.com>
2022-10-12 19:02:02 +0100

    creating test with jests mockResolvedValue - part 1

--- tests/unit/components/jobresults/JobListings.test.js
+++ tests/unit/components/jobresults/JobListings.test.js
@@ -1,4 +1,13 @@
-import axios from "axious";
+import { shallowMount } from "@vue/test-utils";
+import axios from "axios";
 jest.mock("axios");
 
-axios.get();
+import JobListings from "@/components/jobresults/JobListings.vue";
+
+describe("JobListings", () => {
+  it("fetchez jobs", () => {
+    axios.get.mockResolvedValue({ data: [] });
+    shallowMount(JobListings);
+    expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
+  });
+});
