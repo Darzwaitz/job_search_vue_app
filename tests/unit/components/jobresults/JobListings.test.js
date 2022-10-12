bfd97219f19b1a6971078fb2b8b276a3889e2ba1
darzwaitz <darzwaitz@yahoo.com>
2022-10-12 23:55:08 +0100

    creating test with jests mockResolvedValue - part 2

--- tests/unit/components/jobresults/JobListings.test.js
+++ tests/unit/components/jobresults/JobListings.test.js
@@ -10,4 +10,11 @@ describe("JobListings", () => {
     shallowMount(JobListings);
     expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
   });
+
+  it("createz a job listing for each received job", () => {
+    axios.get.mockResolvedValue({ data: Array(15) });
+    const wrapper = shallowMount(JobListings);
+    const jobListings = wrapper.findAll("[data-test='job-listing']");
+    expect(jobListings).toHaveLength(15);
+  });
 });
