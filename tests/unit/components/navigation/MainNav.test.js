d2f130db3f72e7a7d3dd9d474422b2a8e5bbf8ed
darzwaitz <darzwaitz@yahoo.com>
2022-10-27 20:08:51 +0100

    updating MainNav.test file - refactor and rejig

--- tests/unit/components/navigation/MainNav.test.js
+++ tests/unit/components/navigation/MainNav.test.js
@@ -4,9 +4,11 @@ import { createStore } from "vuex";
 import MainNav from "@/components/navigation/MainNav.vue";
 
 describe("MainNav", () => {
-  const createConfig = (store) => ({
+  const createConfig = ($store) => ({
     global: {
-      plugins: [store],
+      mocks: {
+        $store,
+      },
       stubs: {
         "router-link": RouterLinkStub,
       },
@@ -14,17 +16,25 @@ describe("MainNav", () => {
   });
 
   it("displays company name", () => {
-    const store = createStore();
+    const $store = {
+      state: {
+        isLoggedIn: false,
+      },
+    };
     // wrapper as title is used as convention in Vue because it is the wrapper or container
-    const wrapper = shallowMount(MainNav, createConfig(store));
+    const wrapper = shallowMount(MainNav, createConfig($store));
     expect(wrapper.text()).toMatch("Careers");
   });
 
   it("displays menu items for navigation", () => {
-    const store = createStore();
+    const $store = {
+      state: {
+        isLoggedIn: false,
+      },
+    };
 
     // [] within findAll, used as part of test utils
-    const wrapper = shallowMount(MainNav, createConfig(store));
+    const wrapper = shallowMount(MainNav, createConfig($store));
 
     const navigationMenuItemz = wrapper.findAll(
       "[data-test='main-nav-list-item']"
@@ -43,12 +53,33 @@ describe("MainNav", () => {
 
   describe("when user is logged out", () => {
     it("prompts the user to sign in", () => {
-      const store = createStore();
+      const $store = {
+        state: {
+          isLoggedIn: false,
+        },
+      };
 
-      const wrapper = shallowMount(MainNav, createConfig(store));
+      const wrapper = shallowMount(MainNav, createConfig($store));
 
       const loginButton = wrapper.find("[data-test='login-button']");
       expect(loginButton.exists()).toBe(true);
+    });
+
+    it("issuez call to vuex to login user", async () => {
+      const commit = jest.fn();
+      const $store = {
+        state: {
+          isLoggedIn: false,
+        },
+        commit,
+      };
+
+      const wrapper = shallowMount(MainNav, createConfig($store));
+      const loginButton = wrapper.find("[data-test='login-button']");
+
+      await loginButton.trigger("click");
+
+      expect(commit).toHaveBeenCalledWith("LOGIN_USER");
     });
   });
 
@@ -67,29 +98,15 @@ describe("MainNav", () => {
     });
 
     it("displayz subnavigation menu with additional information", async () => {
-      const store = createStore({
-        state() {
-          return {
-            isLoggedIn: true,
-          };
+      const $store = {
+        state: {
+          isLoggedIn: false,
         },
-      });
-      const wrapper = shallowMount(MainNav, createConfig(store));
+      };
+      const wrapper = shallowMount(MainNav, createConfig($store));
 
       const subnav = wrapper.find("[data-test='subnav']");
       expect(subnav.exists()).toBe(true);
-    });
-  });
-
-  describe("when users is logged out", () => {
-    it("issuez call to vuex to login user", async () => {
-      const store = createStore();
-      const commit = jest.fn();
-      store.commit = commit;
-      const wrapper = shallowMount(MainNav, createConfig(store));
-      const loginButton = wrapper.find("[data-test='login-button']");
-
-      await loginButton.trigger("click");
     });
   });
 });
