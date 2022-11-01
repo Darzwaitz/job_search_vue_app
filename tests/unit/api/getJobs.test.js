50a0ea363ec8c2d067e004f10df2008ab3ebd57e
darzwaitz <darzwaitz@yahoo.com>
2022-11-01 23:13:17 +0000

    updating getJobs unit test

--- tests/unit/api/getJobs.test.js
+++ tests/unit/api/getJobs.test.js
@@ -6,5 +6,6 @@ import getJobs from "@/api/getJobs";
 describe("getJobs", () => {
   it("fetchez jobz that candidtates can apply to", async () => {
     await getJobs();
+    expect(axios.get).toHaveBeenCalledWith("http://temp.com/jobs");
   });
 });
