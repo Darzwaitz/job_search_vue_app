648a50ec73bde0ba68ba5face05c39ae1b1c71fb
darzwaitz <darzwaitz@yahoo.com>
2022-12-09 18:51:09 +0000

    installing tailwind and config files

---
+++ Job search app V2/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts}"], // any number of directories down - any file ending with these extensionz (Vue, Js,Ts)
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
