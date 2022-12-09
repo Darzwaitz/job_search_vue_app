db65deb79360ad10ab71b5e47ef8d7c0f41698eb
darzwaitz <darzwaitz@yahoo.com>
2022-12-09 21:20:13 +0000

    adding external font and further config of tailwind

--- Job search app V2/tailwind.config.js
+++ Job search app V2/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme"); // required used here instead of import, as import can't be used
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts}"], // any number of directories down - any file ending with these extensionz (Vue, Js,Ts)
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", ...defaultTheme.fontFamily.sans], // including new external font, along with original fonts from tailwind
+      },
+    },
   },
   plugins: [],
 };
