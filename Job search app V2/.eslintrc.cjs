962456b7274830e583b8079d94e49b136338c196
darzwaitz <darzwaitz@yahoo.com>
2022-12-13 23:21:57 +0000

    installed vitest eslint plugin & setup vitest globals

--- Job search app V2/.eslintrc.cjs
+++ Job search app V2/.eslintrc.cjs
@@ -8,8 +8,12 @@ module.exports = {
     "eslint:recommended",
     "prettier",
     // "@vue/eslint-config-prettier",
+    "plugin:vitest-globals/recommended",
   ],
   parserOptions: {
     ecmaVersion: "latest",
+  },
+  env: {
+    "vitest-globals/env": true,
   },
 };
