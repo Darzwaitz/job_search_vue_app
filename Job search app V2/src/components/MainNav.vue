62f92352b5051236470b442134f5e9ee1de5774f
darzwaitz <darzwaitz@yahoo.com>
2022-12-14 20:57:32 +0000

    added tests folder and setup.js for config & vite config updated

--- Job search app V2/src/components/MainNav.vue
+++ Job search app V2/src/components/MainNav.vue
@@ -5,7 +5,6 @@
         class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
       >
         <a class="flex h-full items-center text-xl" href="/">{{ company }}</a>
-        <h2 class="ml-8 flex h-full items-center">Developed by {{ author }}</h2>
       </div>
     </div>
   </header>
@@ -17,7 +16,6 @@ export default {
   data() {
     return {
       company: "Job Searcher",
-      author: "Darz",
     };
   },
 };
