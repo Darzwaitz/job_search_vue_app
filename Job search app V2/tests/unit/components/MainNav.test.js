a59593617e0f2dec529211aedb1021a045923673
darzwaitz <darzwaitz@yahoo.com>
2022-12-16 13:30:18 +0000

    added MainNav test, to test through Aria Role with getAllByRole if all required listitems - navigation items, are present

--- Job search app V2/tests/unit/components/MainNav.test.js
+++ Job search app V2/tests/unit/components/MainNav.test.js
@@ -8,4 +8,21 @@ describe("MainNav", () => {
     const companyName = screen.getByText("Job Searcher");
     expect(companyName).toBeInTheDocument();
   });
+
+  it("displays menu items for navigation", () => {
+    render(MainNav);
+    const navigationMenuItems = screen.getAllByRole("listitem");
+    const navigationMenuTexts = navigationMenuItems.map(
+      (item) => item.textContent
+    );
+
+    expect(navigationMenuTexts).toEqual([
+      "Teamz",
+      "Locationz",
+      "Life @ here",
+      "How we hire",
+      "Studentz",
+      "Jobz",
+    ]);
+  });
 });
