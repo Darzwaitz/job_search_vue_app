4021c4b5dd562e37321bc538f668b13a480930ad
darzwaitz <darzwaitz@yahoo.com>
2022-12-19 15:18:29 +0000

    add tailwind box-shadow to config, & using inline hover attr to mark style to display only on hover

--- Job search app V2/src/components/ActionButton.vue
+++ Job search app V2/src/components/ActionButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="bg-brand-blue-1 rounded border-0 px-5 py-3 font-medium text-white"
+    class="rounded border-0 bg-brand-blue-1 px-5 py-3 font-medium text-white hover:shadow-blue"
   >
     Sign in
   </button>
