2f727ad1ab1dac22080a3b999bb0862578bd3bd1
darzwaitz <darzwaitz@yahoo.com>
2022-12-23 11:31:58 +0000

    adding fontawesome to dependenciez & registering it for global components in main.js

--- Job search app V2/src/main.js
+++ Job search app V2/src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from "vue";
-import "@/index.css"; // neccessary for tailwind
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+// import magnifying glass
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
+import "@/index.css"; // neccessary for tailwind
 import App from "@/App.vue";
 
-createApp(App).mount("#app");
+library.add(faSearch);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
