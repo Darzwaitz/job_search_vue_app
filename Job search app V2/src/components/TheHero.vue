46cce47b75b775423bb36cec27f6ca584b644bfd
darzwaitz <darzwaitz@yahoo.com>
2023-01-02 20:07:21 +0000

    add temp style to hero section

--- Job search app V2/src/components/TheHero.vue
+++ Job search app V2/src/components/TheHero.vue
@@ -1,6 +1,8 @@
 <template>
   <main>
-    <section>Hero section</section>
+    <section class="flex h-screen flex-col bg-yellow-200 pt-10 pb-20">
+      Hero section
+    </section>
   </main>
 </template>
 
