10f98052ed36b84fc4718fddc3ee875a176e8552
darzwaitz <darzwaitz@yahoo.com>
2023-01-03 16:14:59 +0000

    using created() method - vue life cycle hook

---
+++ Job search app V2/src/components/TheHeadline.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h1>Build for everyone</h1>
+    <h2>Find your next projekt</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "TheHeadline",
+  data() {
+    return {
+      sample: "Hay",
+    };
+  },
+  created() {
+    console.log("created hook existz", this.sample);
+  },
+};
+</script>
