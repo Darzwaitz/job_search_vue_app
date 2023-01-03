378f2d53820d9d0be0ea2829a6fd72472aec1452
darzwaitz <darzwaitz@yahoo.com>
2023-01-03 16:29:26 +0000

    dynamic headline, word switching in title - Part 2

--- Job search app V2/src/components/TheHeadline.vue
+++ Job search app V2/src/components/TheHeadline.vue
@@ -11,14 +11,18 @@ export default {
   data() {
     return {
       action: "Build",
+      interval: null,
     };
   },
   created() {
     this.changeTitle();
   },
+  beforeUnmount() {
+    clearInterval(this.interval);
+  },
   methods: {
     changeTitle() {
-      setInterval(() => {
+      this.interval = setInterval(() => {
         const actions = ["Build", "Create", "Design", "Code"];
         const currentActionIndex = actions.indexOf(this.action);
         const nextActionIndex = (currentActionIndex + 1) % 4;
