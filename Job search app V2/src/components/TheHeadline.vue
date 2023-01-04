17e5aec7c61e1bc911d65afa80c078427e13372f
darzwaitz <darzwaitz@yahoo.com>
2023-01-04 22:35:14 +0000

    css styles added to dynamic title

--- Job search app V2/src/components/TheHeadline.vue
+++ Job search app V2/src/components/TheHeadline.vue
@@ -1,7 +1,8 @@
 <template>
   <section>
-    <h1>
-      <span :class="actionClasses">{{ action }}</span> <br />
+    <h1 class="mb-14 text-8xl font-bold tracking-tighter">
+      <span :class="actionClasses">{{ action }}</span>
+      <br />
       for everyone
     </h1>
     <h2>Find your next projekt</h2>
