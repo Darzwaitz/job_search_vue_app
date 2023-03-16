6dd416f1bbe78d3e56f8889b2be776e3ff8b5dae
darzwaitz <darzwaitz@yahoo.com>
2023-03-16 18:28:37 +0000

    using new TextInput comp & deleting previously used code for that

--- Job search app V2/src/components/JobSearch/JobSearchForm.vue
+++ Job search app V2/src/components/JobSearch/JobSearchForm.vue
@@ -6,12 +6,7 @@
     <div class="flex h-full flex-1 flex-nowrap text-base font-light">
       <div class="relative flex h-full flex-1 items-center pr-3">
         <label for="" class="absolute left-0 -top-10">Role</label>
-        <input
-          v-model="role"
-          type="text"
-          placeholder="Software Engineer"
-          class="w-full text-lg font-normal focus:outline-none"
-        />
+        <text-input placeholder="Software Engineer" />
       </div>
 
       <span
@@ -21,12 +16,7 @@
 
       <div class="relative flex h-full flex-1 items-center pl-3">
         <label for="" class="absolute left-0 -top-10">Where?</label>
-        <input
-          v-model="location"
-          type="text"
-          placeholder="Lisbon"
-          class="w-full text-lg font-normal focus:outline-none"
-        />
+        <text-input placeholder="Lisbon" />
       </div>
     </div>
 
@@ -36,9 +26,10 @@
 
 <script>
 import ActionButton from "@/components/Shared/ActionButton.vue";
+import TextInput from "@/components/Shared/TextInput.vue";
 export default {
   name: "JobSearchForm",
-  components: { ActionButton },
+  components: { ActionButton, TextInput },
   data() {
     return {
       role: "",
