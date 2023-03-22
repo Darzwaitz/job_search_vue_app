f9be4ed66c47703bb0d52f6357d304b298cb2d17
darzwaitz <darzwaitz@yahoo.com>
2023-03-22 22:16:28 +0000

    creating test for TextInput comp - TBU

---
+++ Job search app V2/tests/unit/components/Shared/TextInput.test.js
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/vue";
+import userEvent from "@testing-library/user-event";
+
+import TextInput from "@/components/Shared/TextInput.vue";
+
+describe("TextInput", () => {
+  it("communicatez that user haz entered character", async () => {
+    const { emitted } = render(TextInput, {
+      props: {
+        modelValue: "",
+      },
+    });
+    const input = screen.getByRole("textbox");
+    console.log(emitted());
+    await userEvent.type(input, "N");
+    // emitted()["update:modelValue"];
+  });
+});
