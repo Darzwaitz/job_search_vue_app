14522edf5b35fae2ec5b8de22dbe19e13d14f8f6
darzwaitz <darzwaitz@yahoo.com>
2023-03-27 21:58:48 +0100

    moving content into new comp routes

--- Job search app V2/src/App.vue
+++ Job search app V2/src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/jobs/results">Job Resultz</router-link>
-  </div>
+  <main-nav />
   <router-view />
 </template>
 
 <script>
-// import MainNav from "@/components/Navigation/MainNav.vue";
+import MainNav from "@/components/Navigation/MainNav.vue";
 // import TheHero from "@/components/JobSearch/TheHero.vue";
 export default {
   name: "App",
+  components: { MainNav },
 };
 </script>
