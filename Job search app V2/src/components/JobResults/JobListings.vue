847a11ccca0be1c197a03d62705c4a282d18b9d6
darzwaitz <darzwaitz@yahoo.com>
2023-04-04 23:05:43 +0100

    adding job title & location & css styles in job listings comp - UPDATED

--- Job search app V2/src/components/JobResults/JobListings.vue
+++ Job search app V2/src/components/JobResults/JobListings.vue
@@ -7,8 +7,18 @@
           class="mx-auto block rounded border border-solid border-brand-gray-2 bg-white hover:shadow-gray"
         >
           <div class="mx-8 border-b border-solid border-brand-gray-2 pt-5 pb-2">
-            <h2 class="mb-2 text-2xl"></h2></div
-        ></router-link>
+            <h2 class="mb-2 text-2xl">Technical program manager</h2>
+            <div class="flex flex-row align-middle">
+              <div class="mr-5">
+                <span>Company</span>
+              </div>
+
+              <div class="">
+                <span>San Francisco, USA</span>
+              </div>
+            </div>
+          </div></router-link
+        >
       </li>
     </ol>
   </main>
