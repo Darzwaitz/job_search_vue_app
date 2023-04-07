4396c9ed1e8dbc551ded128e258a10c2a353f01c
darzwaitz <darzwaitz@yahoo.com>
2023-04-07 15:37:09 +0100

    rendering job listing per job in array with computed property

--- Job search app V2/src/components/JobResults/JobListing.vue
+++ Job search app V2/src/components/JobResults/JobListing.vue
@@ -1,18 +1,26 @@
 <template>
   <li class="mb-7">
     <router-link
-      to="/jobs/results/1"
+      :to="jobPageLink"
       class="mx-auto block rounded border border-solid border-brand-gray-2 bg-white hover:shadow-gray"
     >
       <div class="mx-8 border-b border-solid border-brand-gray-2 pt-5 pb-2">
-        <h2 class="mb-2 text-2xl">Technical program manager</h2>
+        <h2 class="mb-2 text-2xl">{{ job.title }}</h2>
         <div class="flex flex-row align-middle">
           <div class="mr-5">
-            <span>Company</span>
+            <span>{{ job.organization }}</span>
           </div>
 
           <div>
-            <span>San Francisco, USA</span>
+            <ul>
+              <li
+                v-for="location in job.locations"
+                :key="location"
+                class="mr-5 inline-block"
+              >
+                <span>{{ location }}</span>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -22,15 +30,18 @@
           <h3 class="mt-1 mb-2">Qualifications:</h3>
           <div>
             <ul class="list-disc pl-8">
-              <li>Bachelors degree or equiv.</li>
-              <li>5 yearz experience</li>
-              <li>Experience analysing data through querying databases</li>
+              <li
+                v-for="qualification in job.minimumQualifications"
+                :key="qualification"
+              >
+                {{ qualification }}
+              </li>
             </ul>
           </div>
         </div>
 
         <div class="mt-2 text-center">
-          <router-link to="/jobs/results/1" class="text-brand-blue-1">
+          <router-link :to="jobPageLink" class="text-brand-blue-1">
             Expand
           </router-link>
         </div>
@@ -42,5 +53,16 @@
 <script>
 export default {
   name: "JobListing",
+  props: {
+    job: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    jobPageLink() {
+      return `/jobs/results/${this.job.id}`;
+    },
+  },
 };
 </script>
