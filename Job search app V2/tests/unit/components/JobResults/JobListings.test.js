e61ba6589e42ed54fa82c3d4cbc7d5966afe181a
darzwaitz <darzwaitz@yahoo.com>
2023-04-14 12:30:43 +0100

    fixing and updating unit test on JobListings comp

--- Job search app V2/tests/unit/components/JobResults/JobListings.test.js
+++ Job search app V2/tests/unit/components/JobResults/JobListings.test.js
@@ -9,15 +9,28 @@ vi.mock("axios");
 describe("JobListings", () => {
   it("fetchez jobz", () => {
     axios.get.mockResolvedValue({ data: [] });
-    render(JobListings);
+    const $route = { query: { page: "5" } };
+
+    render(JobListings, {
+      global: {
+        mocks: {
+          $route,
+        },
+      },
+    });
+
     expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
   });
 
   it("it displayz max of 10 jobz", async () => {
     axios.get.mockResolvedValue({ data: Array(10).fill({}) });
+    const $route = { query: { page: "1" } };
 
     render(JobListings, {
       global: {
+        mocks: {
+          $route,
+        },
         stubs: {
           RouterLink: RouterLinkStub,
         },
