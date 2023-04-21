7248ddd2babf5b2191645a6f5acc506de7a724c9
darzwaitz <darzwaitz@yahoo.com>
2023-04-21 12:19:49 +0100

    adding to unit test to check next page link doesnt show on last page

--- Job search app V2/tests/unit/components/JobResults/JobListings.test.js
+++ Job search app V2/tests/unit/components/JobResults/JobListings.test.js
@@ -93,4 +93,20 @@ describe("JobListings", () => {
       expect(nextLink).toBeInTheDocument();
     });
   });
+
+  describe("when user is on last page", () => {
+    it("does not show link to next page", async () => {
+      axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+      const queryParams = { page: "2" };
+      const $route = createRoute(queryParams);
+
+      renderJobListings($route);
+
+      await screen.findAllByRole("listitem");
+      const nextLink = screen.queryByRole("link", {
+        name: /next/i,
+      });
+      expect(nextLink).not.toBeInTheDocument();
+    });
+  });
 });
