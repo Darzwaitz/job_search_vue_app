bea9ae235814bf15f64af3150aa5a37717be89a8
darzwaitz <darzwaitz@yahoo.com>
2023-04-25 13:17:36 +0100

    changing isLoggedIn evaluations with pinia store value

--- Job search app V2/src/components/Navigation/MainNav.vue
+++ Job search app V2/src/components/Navigation/MainNav.vue
@@ -26,16 +26,20 @@
           </ul>
         </nav>
         <div class="ml-auto flex h-full items-center">
-          <profile-image v-if="isLoggedIn" />
+          <profile-image v-if="uuserStore.isLoggedIn" />
           <action-button v-else text="Sign In" @click="loginUser" />
         </div>
       </div>
-      <the-subnav v-if="isLoggedIn" />
+      <the-subnav v-if="userStore.isLoggedIn" />
     </div>
   </header>
 </template>
 
 <script>
+import { mapStores } from "pinia";
+
+import { useUserStore } from "@/stores/user";
+
 import ActionButton from "@/components/Shared/ActionButton.vue";
 import ProfileImage from "@/components/Navigation/ProfileImage.vue";
 import TheSubnav from "@/components/Navigation/TheSubnav.vue";
@@ -57,17 +61,21 @@ export default {
         { text: "Studentz", url: "/" },
         { text: "Jobz", url: "/jobs/results" },
       ],
-      isLoggedIn: false,
     };
   },
   computed: {
+    ...mapStores(useUserStore),
     headerHeightClass() {
-      return { "h-16": !this.isLoggedIn, "h-32": this.isLoggedIn };
+      // userStore name is created by pinia in store file - concatenates defined name in defineStore() method
+      return {
+        "h-16": !this.userStore.isLoggedIn,
+        "h-32": this.userStore.isLoggedIn,
+      };
     },
   },
   methods: {
     loginUser() {
-      this.isLoggedIn = !this.isLoggedIn;
+      // this.userStore.isLoggedIn = !this.userStore.isLoggedIn;
     },
   },
 };
