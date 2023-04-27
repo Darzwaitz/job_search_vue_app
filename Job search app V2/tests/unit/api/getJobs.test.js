16a3cfe1eccaa051bf8c0dcdc51e1ef8c945910d
darzwaitz <darzwaitz@yahoo.com>
2023-04-27 20:35:04 +0100

    created component based on unit test for TDD approach - part 2

--- Job search app V2/tests/unit/api/getJobs.test.js
+++ Job search app V2/tests/unit/api/getJobs.test.js
@@ -5,8 +5,29 @@ import getJobs from "@/api/getJobs";
 vi.mock("axios");
 
 describe("getJobs", () => {
-  it("fetchez job candidatez can apply to", async () => {
+  beforeEach(() => {
+    axios.get.mockResolvedValue({
+      data: [
+        {
+          id: 1,
+          title: "Python Engineer",
+        },
+      ],
+    });
+  });
+
+  it("fetchez jobz candidatez can apply to", async () => {
     await getJobs();
     expect(axios.get).toHaveBeenCalledWith("http://mytestsite.com/jobs");
+  });
+
+  it("extractz jobz from response", async () => {
+    const jobs = await getJobs();
+    expect(jobs).toEqual([
+      {
+        id: 1,
+        title: "Python Engineer",
+      },
+    ]);
   });
 });
