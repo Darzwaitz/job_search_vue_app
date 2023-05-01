e20b057fbe709262985ddf9eb22d5321343a4c69
darzwaitz <darzwaitz@yahoo.com>
2023-05-01 16:12:59 +0100

    added 2 icons from fontawesome library

--- Job search app V2/src/main.js
+++ Job search app V2/src/main.js
@@ -3,12 +3,18 @@ import { createPinia } from "pinia";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 // import magnifying glass icon
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faAngleDown,
+  faAngleUp,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
 
 import "@/index.css"; // neccessary for tailwind
 import router from "@/router";
 import App from "@/App.vue";
 
+library.add(faAngleDown);
+library.add(faAngleUp);
 library.add(faSearch);
 
 const pinia = createPinia();
