f0a0a49d15af9eccc48ed7e944ac57cd1df868d1
darzwaitz <darzwaitz@yahoo.com>
2023-05-01 16:32:02 +0100

    adding styles and other content to collapsible accordian - part 1

--- Job search app V2/src/components/Shared/CollapsibleAccordian.vue
+++ Job search app V2/src/components/Shared/CollapsibleAccordian.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>My Accordian</div>
+  <div class="border-b border-solid border-brand-gray-2 py-5">
+    <div
+      class="flex cursor-pointer flex-wrap items-center justify-between"
+      @click="open"
+    ></div>
+
+    <div v-if="isOpen" class="">Child</div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "CollapsibleAccordian",
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  methods: {
+    open() {
+      this.isOpen = !this.isOpen;
+    },
+  },
 };
 </script>
