449c0d7722ada9f89f67459133c9e4e2f5572c15
darzwaitz <darzwaitz@yahoo.com>
2023-05-03 17:52:21 +0100

    writing a unit:test for slot

---
+++ Job search app V2/tests/unit/components/Shared/CollapsibleAccoridan.test.js
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/vue";
+import userEvent from "@testing-library/user-event";
+
+import CollapsibleAccordian from "@/components/Shared/CollapsibleAccordian.vue";
+
+describe("CollapsibleAccordian", () => {
+  it("renders child content", async () => {
+    render(CollapsibleAccordian, {
+      global: {
+        stubs: {
+          FontAwesomeIcon: true,
+        },
+      },
+      props: {
+        header: "My Category",
+      },
+      slots: {
+        default: "<h3>My nested child</h3>",
+      },
+    });
+
+    expect(screen.queryByText("My nested child")).not.toBeInTheDocument();
+    const button = screen.getByRole("button", { name: /my category/i });
+    await userEvent.click(button);
+    expect(screen.getByText("My nested child")).toBeInTheDocument();
+  });
+});
