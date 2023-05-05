070af0a2253f1cb057de01b731ac90eb9e541cfa
darzwaitz <darzwaitz@yahoo.com>
2023-05-05 22:40:58 +0100

    creating a named slot - Part 2

--- Job search app V2/src/views/TeamsView.vue
+++ Job search app V2/src/views/TeamsView.vue
@@ -1,5 +1,13 @@
 <template>
-  <header-container />
+  <header-container>
+    <template #title>
+      <h1 class="w-full text-4xl font-normal">Teamz</h1>
+    </template>
+
+    <template #subtitle>
+      <h2 class="my-4 w-full text-base font-light">A subtitle heading</h2>
+    </template>
+  </header-container>
 </template>
 
 <script>
