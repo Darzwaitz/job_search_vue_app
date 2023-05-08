9fedc35eb6d9df735e5df0a0fd80549777afb381
darzwaitz <darzwaitz@yahoo.com>
2023-05-08 21:45:50 +0100

    new comp created - will fetch with axios, content called spotLights

---
+++ Job search app V2/src/components/JobSearch/SpotLight.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "SpotLight",
+  data() {
+    return {
+      spotLights: [],
+    };
+  },
+  async mounted() {
+    const baseUrl = import.meta.env.VITE_APP_API_URL;
+    const url = `${baseUrl}/spotLights`;
+    const response = await axios.get(url);
+    this.spotLights = response.data;
+  },
+};
+</script>
